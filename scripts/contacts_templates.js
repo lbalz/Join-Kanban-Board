e4463e8e88d6fe059463baba12335f72dc351187
Ben Dei <Xelnarash@googlemail.com>
2024-10-22 17:33:29 +0200

    css improvment for concat html site

--- scripts/contacts_templates.js
+++ scripts/contacts_templates.js
@@ -44,12 +44,12 @@ function contactDetailsRenderTemplate(singleUser, index) {
         </div>
     </div>
     <div>
-        <h2>Contact Information</h2>
-        <div>
+        <h2 class ="h2ForContactInformation">Contact Information</h2>
+        <div class="EmailInContactInformation">
             <span style="font-weight: bold">Email</span> <br>
             <span style="color: #4589FF">${singleUser.email}</span>
         </div>
-        <div>
+        <div class="PhoneInContactInformation">
             <span style="font-weight: bold">Phone</span> <br>
             <span>${singleUser.phone}</span>
         </div>
