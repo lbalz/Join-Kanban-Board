97a6d14b1f7b6c60e3e30cd0e878ba29bb32065a
Bilal Mohammed <kurdo1ka@gmail.com>
2024-10-23 18:36:15 +0200

    fiexd some elemnts

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -1,119 +1,60 @@
-// function setActive(button) {
-//   // Get all buttons with the class 'btn'
-//   const buttons = document.getElementsByClassName('priorityBtn');
+const urgent = document.getElementById('urgent');
+const medium = document.getElementById('medium');
+const low = document.getElementById('low');
 
-
-//   const mediumHover = document.getElementById('medium');
-//   const lowHover = document.getElementById('low');
-//   // Remove 'active' class from all buttons
-//   for (let i = 0; i < buttons.length; i++) {
-//     buttons[i].classList.remove('active');
-//   }
-
-//   // Add the 'active' class to the clicked button
-//   button.classList.add('active');
-
-//   // Remove Hover 
-//   mediumHover.classList.toggle('mediumHover');
-//   lowHover.classList.toggle('lowHover');
-// }
 
 function toggleUrgent() {
-  const urgentHover = document.getElementById('urgent');
-  const mediumHover = document.getElementById('medium');
-  const lowHover = document.getElementById('low');
-  
-  if (urgentHover.classList.contains('active')) {
-    urgentHover.classList.remove('active');
-    urgentHover.classList.add('urgentHover');
-    mediumHover.classList.add('mediumHover');
-    lowHover.classList.add('lowHover');
+  if (urgent.classList.contains('active')) {
+    urgent.classList.remove('active');
+    urgent.classList.add('urgentHover');
+    medium.classList.add('mediumHover');
+    low.classList.add('lowHover');
   } else {
-    urgentHover.classList.add('active');
-    urgentHover.classList.remove('urgentHover');
-    mediumHover.classList.add('mediumHover');
-    lowHover.classList.add('lowHover');
-    lowHover.classList.remove('active');
-    mediumHover.classList.remove('active');
+    urgent.classList.add('active');
+    urgent.classList.remove('urgentHover');
+    medium.classList.add('mediumHover');
+    low.classList.add('lowHover');
+    low.classList.remove('active');
+    medium.classList.remove('active');
   }
-
-  // urgentHover.classList.toggle('urgentHover');
-  // urgentHover.classList.toggle('active');
-  // if (!urgentHover.classList.contains('active')) {
-  //   document.getElementById('medium').classList.remove('active');
-  //   document.getElementById('low').classList.remove('active');
-  //   urgentHover.classList.add('active');
-  //   urgentHover.classList.remove('urgentHover');
-  // }  else {
-  //   urgentHover.classList.add('urgentHover');
-  // }
 }
 
-function toggleMedium() {
-  const mediumHover = document.getElementById('medium');
-  const urgentHover = document.getElementById('urgent');
-  const lowHover = document.getElementById('low');
 
-  if (mediumHover.classList.contains('active')) {
-    mediumHover.classList.remove('active');
-    mediumHover.classList.add('mediumHover');
-    urgentHover.classList.add('urgentHover');
-    lowHover.classList.add('lowHover');
+function toggleMedium() {
+  if (medium.classList.contains('active')) {
+    medium.classList.remove('active');
+    medium.classList.add('mediumHover');
+    urgent.classList.add('urgentHover');
+    low.classList.add('lowHover');
   } else {
-    mediumHover.classList.add('active');
-    mediumHover.classList.remove('mediumHover');
-    urgentHover.classList.add('urgentHover');
-    lowHover.classList.add('lowHover');
-    urgentHover.classList.remove('active');
-    lowHover.classList.remove('active');
+    medium.classList.add('active');
+    medium.classList.remove('mediumHover');
+    urgent.classList.add('urgentHover');
+    low.classList.add('lowHover');
+    urgent.classList.remove('active');
+    low.classList.remove('active');
   }
-  
-  // mediumHover.classList.toggle('mediumHover');
-  // mediumHover.classList.toggle('active');
-  // if (!mediumHover.classList.contains('active')) {
-  //   document.getElementById('urgent').classList.remove('active');
-  //   document.getElementById('low').classList.remove('active');
-  //   mediumHover.classList.add('active');
-  //   mediumHover.classList.remove('mediumHover');
-  // } else {
-  //   mediumHover.classList.add('mediumHover');
-  // }
 }
 
 
 function toggleLow() {
-  const lowHover = document.getElementById('low');
-  const mediumHover = document.getElementById('medium');
-  const urgentHover = document.getElementById('urgent');
-  
-  if (lowHover.classList.contains('active')) {
-    lowHover.classList.remove('active');
-    lowHover.classList.add('lowHover');
-    urgentHover.classList.add('urgentHover');
-    mediumHover.classList.add('mediumHover');
+  if (low.classList.contains('active')) {
+    low.classList.remove('active');
+    low.classList.add('lowHover');
+    urgent.classList.add('urgentHover');
+    medium.classList.add('mediumHover');
   } else {
-    lowHover.classList.add('active');
-    lowHover.classList.remove('lowHover');
-    urgentHover.classList.add('urgentHover');
-    mediumHover.classList.add('mediumHover');
-    urgentHover.classList.remove('active');
-    mediumHover.classList.remove('active');
+    low.classList.add('active');
+    low.classList.remove('lowHover');
+    urgent.classList.add('urgentHover');
+    medium.classList.add('mediumHover');
+    urgent.classList.remove('active');
+    medium.classList.remove('active');
   }
-
-  // lowHover.classList.toggle('lowHover');
-  // lowHover.classList.toggle('active');
-  // if (!lowHover.classList.contains('active')) {
-  //   document.getElementById('urgent').classList.remove('active');
-  //   document.getElementById('medium').classList.remove('active');
-  //   lowHover.classList.add('active');
-  //   lowHover.classList.remove('lowHover');
-  // }  else {
-  //   lowHover.classList.add('lowHover');
-  // }
 }
 
 
-// Toggle Dropdown of assigned Contacts
+//! Toggle Dropdown of assigned Contacts
 function toggleDropdownAssignedContacts() {
   const assignedContacts = document.getElementById('assignedContacts');
   // Toggle the 'hidden' class on or off
@@ -138,8 +79,7 @@ function changeDropdownAssignedIcon() {
 }
 
 
-
-// Toggle Dropdown of Category
+//! Toggle Dropdown of Category
 function toggleDropdownCategory() {
   const categoryTasks = document.getElementById('categoryTasks');
   // Toggle the 'hidden' class on or off
