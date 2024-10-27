f00d94b2ce9d05736f28369d174e1ba510d383c6
Can Oeter <c.oeter@live.com>
2024-10-27 23:27:07 +0100

    Can
    Summary inklusive aktuelle Datumsanzeige

--- script.js
+++ script.js
@@ -2,3 +2,21 @@
 function toggledropDownBar(){
     document.getElementById('dropDownBar').classList.toggle('activ')
 }
+
+// Funktion aktuelle Datum
+function displayCurrentDate() {
+    const dateElement = document.getElementById('currentDate');
+    
+    // Aktuelles Datum 
+    const today = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    
+    // Datum formatieren hier für DT: 'de-DE'
+    const formattedDate = today.toLocaleDateString('en-US', options);
+    
+    // Datum in das HTML-Element einsetzen
+    dateElement.textContent = formattedDate;
+}
+
+// Funktion aufrufen, sobald das DOM geladen ist
+window.onload = displayCurrentDate;
